549d42f3ba7fa691751001f27446ac0aed2a682a
mactkg <mactkg@gmail.com>
2014-07-26 16:25:36 +0900

    Logモジュールの実装 - Time::Piece使った

--- perl/lib/Log.pm
+++ perl/lib/Log.pm
@@ -1,6 +1,7 @@
 package Log;
 use strict;
 use warnings;
+use Time::Piece;
 
 sub new {
     my ($class, %args) = @_;
@@ -8,18 +9,33 @@ sub new {
 }
 
 sub protocol {
+  my $self = shift;
+  my @splited = split(/ /, $self->{req});
+  return $splited[2];
 }
 
 sub method {
+  my $self = shift;
+  my @splited = split(/ /, $self->{req});
+  return $splited[0];
 }
 
 sub path {
+  my $self = shift;
+  my @splited = split(/ /, $self->{req});
+  return $splited[1];
 }
 
 sub uri {
+  my $self = shift;
+  my $path = $self->path();
+  return "http://" . $self->{host} . $self->path();
 }
 
 sub time {
+  my $self = shift;
+  my $converted_time = gmtime $self->{epoch};
+  return $converted_time->datetime;
 }
 
 1;
