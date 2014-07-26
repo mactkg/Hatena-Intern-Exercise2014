21a13304cbb66fd4bd2eb2e9ac4dec13f909ab6f
mactkg <mactkg@gmail.com>
2014-07-26 21:11:53 +0900

    Parserを実装

--- perl/lib/Parser.pm
+++ perl/lib/Parser.pm
@@ -1,6 +1,7 @@
 package Parser;
 use strict;
 use warnings;
+use Log;
 
 sub new {
     my ($class, %args) = @_;
@@ -8,6 +9,21 @@ sub new {
 }
 
 sub parse {
+  my $self = shift;
+  open my $fh, '<', $self->{filename} or die $!;
+  my @lines = <$fh>;
+  my @result = map {
+    Log->new(
+      map {
+        chomp($_);
+        my @splited=split(/:/, $_, 2);
+        $splited[0] => $splited[1]
+      } grep {
+        /.:[^\-]./
+      } split(/\t/, $_)
+    );
+  } @lines;
+  return \@result; # not return @result
 }
 
 1;
