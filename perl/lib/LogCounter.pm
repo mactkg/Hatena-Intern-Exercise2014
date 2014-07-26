b7040b570af2beef9e27525bc336a909a4d10d74
mactkg <mactkg@gmail.com>
2014-07-26 23:52:50 +0900

    LogCounter実装 なんとなくデリファレンスとかが多い気がするので実装は見直したいけどとりあえず

--- perl/lib/LogCounter.pm
+++ perl/lib/LogCounter.pm
@@ -1,6 +1,7 @@
 package LogCounter;
 use strict;
 use warnings;
+use Data::Dumper;
 
 sub new {
     my ($class, $logs) = @_;
@@ -8,9 +9,27 @@ sub new {
 };
 
 sub group_by_user {
+  my $self = shift;
+  my $users;
+  map {
+    if(defined($_->{user})) {
+      push(@{ $users->{$_->{user}} }, $_);
+    } else {
+      push(@{ $users->{guest} }, $_);
+    }
+  } @{ $self->{logs} };
+  return $users;
 }
 
 sub count_error {
+  my $self = shift;
+  my $cnt_error = 0;
+  map {
+    if($_->{status} =~ /5[0-9][0-9]/) {
+      $cnt_error++;
+    }
+  } @{ $self->{logs} };
+  return $cnt_error;
 }
 
 1;
