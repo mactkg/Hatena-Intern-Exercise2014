1fe8b85746d09f6d3b3ec5c3c798427168a063d5
mactkg <mactkg@gmail.com>
2014-08-05 00:51:39 +0900

    JS課題2, 3の実装

--- js/js-3.js
+++ js/js-3.js
@@ -1,1 +1,13 @@
 // 課題 JS-3 の実装をここに記述してください。
+
+document.addEventListener("DOMContentLoaded", load, false);
+
+function load() {
+  var submitButton = document.getElementById("submit-button");
+  submitButton.addEventListener("click", onDataSubmited, false);
+};
+
+function onDataSubmited() {
+  var data = document.getElementById("log-input").textContent;
+  createLogTable(document.getElementById("table-container"), parseLTSVLog(data));
+}
--- js/main.js
+++ js/main.js
@@ -2,8 +2,10 @@
 function parseLTSVLog(ltsv) {
   var result = ltsv.split('\n').map(
     function(line){
+      // return dusts
       if(line == "")
         return null;
+
       var hash = {};
       var elements = line.split('\t');
       for(var i = 0; i < elements.length; i++) {
@@ -14,7 +16,8 @@ function parseLTSVLog(ltsv) {
       }
       return hash;
   });
-  
+ 
+  // cleaning
   for(var i = 0; i < result.length; i++) {
     if(result[i] == null) {
       result.splice(i);
@@ -24,3 +27,27 @@ function parseLTSVLog(ltsv) {
 };
 
 // 課題 JS-2: 関数 `createLogTable` を記述してください
+function createLogTable(dom, ltsvs) {
+  var table = document.createElement("table");
+  
+  // make header
+  var header = table.createTHead();
+  var headerRow = header.insertRow();
+  var pathCell = document.createElement("th");
+  pathCell.textContent = "path";
+  headerRow.appendChild(pathCell);
+  var reqtCell = document.createElement("th");
+  reqtCell.textContent = "reqtime_microsec";
+  headerRow.appendChild(reqtCell);
+
+  // table body
+  var tbody = document.createElement("tbody"); 
+  for(var i = 0; i < ltsvs.length; i++) {
+    var row = tbody.insertRow();
+    row.insertCell().textContent = ltsvs[i]["path"];
+    row.insertCell().textContent = ltsvs[i]["reqtime_microsec"];
+  }
+
+  table.appendChild(tbody);
+  dom.appendChild(table);
+}
